5d67ee7f8ce2212806cc7a9156d5e73d7bc22cfa
shwetha1 <shwetha.raghavendra1@pearson.com>
2020-12-09 00:51:51 +0530

    updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import "./Search.css";
 import showImg from "../images/frozen.jpg";
 
 
+
+
 function Search() {
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState('');
+    const [search, setSearch] = useState('');
+    const [dispResult, setDispResult] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(
+            //`http://hn.algolia.com/api/v1/search?query=${search}`,
+            `https://api.tvmaze.com/search/shows?q=walking`
+          );
+     console.log(result.data)
+          setData(result.data);
+          setDispResult(true)
+        };
+     
+        if(search != ''){
+            fetchData();
+        }
+        
+      }, [search]);
+    
+
     return (
         <div>
             <div className="header"><div className="title">Show Finder</div></div>
-            <div>            
+            <div>
                 <div className="container">
-                <input type="text"/><div className="btn-search">Search</div>
-                <div>
-                    <div className="show-details-container">
-                    <div className="show-img"><img alt="showimg" src={showImg} className="show-Img-disp" /></div>
-                    <div className="show-description-container">
-                        <div className="show-title">Show Title</div>
-                        <div className="show-description">Show Title</div>
-                        <div className="btn-show-episode">Show Episodes</div>
+                    <input type="text" value={query}
+        onChange={event => setQuery(event.target.value)} />
+                    <div className="btn-search" onClick={() => setSearch(query)}>Search</div>
+                    <div>
+                        {/* <div className="show-details-container">
+                            <div className="show-img"><img alt="showimg" src="http://static.tvmaze.com/uploads/images/medium_portrait/238/596840.jpg"
+                                className="show-Img-disp" /></div>
+                            <div className="show-description-container">
+                                <div className="show-title">Fear the Walking Dead</div>
+                                <div className="show-description"><p>Living in the same universe as <i>The Walking Dead</i>, <b>Fear the Walking Dead </b>is a gritty drama that explores the onset of the undead apocalypse through the lens of a fractured family. Set in a city where people come to escape, shield secrets, and bury their pasts, a mysterious outbreak threatens to disrupt what little stability high school guidance counselor Madison Clark and English teacher Travis Manawa have managed to assemble. The everyday pressure of blending two families while dealing with resentful, escapist, and strung out children takes a back seat when society begins to break down. A forced evolution, a necessary survival of the fittest takes hold, and our dysfunctional family must either reinvent themselves or embrace their darker histories.</p>
+                                </div>
+                                <div className="btn-show-episode">Show Episodes</div>
+                            </div>
+                        </div> */}
+                      {/* <ul>
+        {data.map(item => (
+          <li key={item.score}>
+            <a href={item.score}>{item.score}</a>
+          </li>
+        ))}
+      </ul> */}
+
+      {data.map(item => (
+          <div className="show-details-container">
+          {/* <div className="show-img"><img alt="showimg" src="http://static.tvmaze.com/uploads/images/medium_portrait/238/596840.jpg"
+              className="show-Img-disp" /></div> */}
+              
+              {item.show.image != null ? <img alt="showimg" src={item.show.image.medium} className="show-Img-disp" /> : ""}
+          <div className="show-description-container">
+              <div className="show-title">{item.show.name}</div>
+              <div className="show-description">{item.show.summary}
+              </div>
+              <div className="btn-show-episode">Show Episodes</div>
+          </div>
+      </div>
+        ))}
+      
+      
+      
                     </div>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
